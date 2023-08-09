44ca21ae1b5145994b92ba0388811f5065786a3c
Juan Diego Lopez <jdlopezg1@eafit.edu.co>
2023-08-08 22:14:52 -0500

    Formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -7,12 +7,7 @@ from .models import Movie
 def home(request):
     #return HttpResponse('<h1> Welcome to Home Page </h1> ')
     searchTerm = request.GET.get('searchMovie')
-    
-    if searchTerm:
-        movies = Movie.objects.filter(title__icontains=searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
 def about(request):
     return render(request, 'about.html')
