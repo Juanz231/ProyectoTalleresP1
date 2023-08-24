b80c2cb329e26055329e3ffe858abb90a2387135
Juan Diego Lopez <jdlopezg1@eafit.edu.co>
2023-08-23 21:10:09 -0500

    Creación del carrusel

--- movie/views.py
+++ movie/views.py
@@ -7,7 +7,11 @@ from .models import Movie
 def home(request):
     #return HttpResponse('<h1> Welcome to Home Page </h1> ')
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'searchTerm':searchTerm})
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
